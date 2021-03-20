76436c6e72fd6c9519138bce30e86c9137fd1dce
Max Shaffer <max@Maxs-MBP.attlocal.net>
2021-03-20 09:19:57 -0400

    updated guessing game

--- 3.2-guessingGame.instructional.js
+++ 3.2-guessingGame.instructional.js
@@ -20,46 +20,70 @@
 
 //NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
 
-let chosenNumber = Math.floor((Math.random() * 10) +1);
-console.log('the chosen number is ' + chosenNumber);
 
+
+// Change these two variables to alter the entire game. 
 let maxAttempts = 3;
 let betweenOneAnd = 10;
 
+
+
+let chosenNumber = Math.floor((Math.random() * betweenOneAnd) +1);
+console.log('the chosen number is ' + chosenNumber);
+
+
 let attemptNumber = [];
+let gameWon = false;
 let guessLog = document.getElementById('guessLog');
 let newLog = document.createElement('p');
 let playAgain = document.getElementById('playAgain');
+let submitButton = document.getElementById('submitButton');
+let range = document.getElementById('range');
+range.innerText = `It is between 1 and ${betweenOneAnd}. You have ${maxAttempts} tries.`;
+
 let validRange = [];
-    for (i=1; i<= betweenOneAnd; i++){
-        validRange.push(i);
-    }
-
-
-function guessNumber(){
-        
-    let userGuess = Number(document.getElementById('userGuess').value); 
-    console.log(userGuess);
-
-    if(validRange.includes(userGuess) == false){
-        alert('Clever, but No.\nNumber must be an integer between 1 and 10');
-    }else if(attemptNumber.includes(userGuess)){
-        alert(`You already guessed ${userGuess}.\n(I'll let it slide.)`)
-    } else if(userGuess == chosenNumber){
-        guessLog.appendChild(newLog);
-        attemptNumber.push(userGuess);
-        newLog.innerText = `${(attemptNumber.length)}) Thats the number! you win!`;
-        playAgain.style = 'display: block'
-    }else if(userGuess < chosenNumber){
-        guessLog.appendChild(newLog);
-        attemptNumber.push(userGuess);
-        newLog.innerText = `${(attemptNumber.length)}) ${userGuess} is too low. Guess higher`;
-    }else if(userGuess > chosenNumber){
-        guessLog.appendChild(newLog);
-        attemptNumber.push(userGuess);
-        newLog.innerText = `${(attemptNumber.length)}) ${userGuess} is too high. Guess lower`;
-    }
-
-    console.log(attemptNumber);
+for (i=1; i<= betweenOneAnd; i++){
+    validRange.push(i);
 }
 
+let lossCheck = () =>{
+    if((attemptNumber.length == maxAttempts) && (gameWon == false)){
+        newLog.innerText = `Sorry, but that's all ${maxAttempts} tries. The number was ${chosenNumber}.`;
+        guessLog.appendChild(newLog);
+        playAgain.style = 'display: block';
+        submitButton.style = 'display: none';
+    }
+}
+    
+    function guessNumber(){
+        
+        let userGuess = Number(document.getElementById('userGuess').value); 
+        console.log(userGuess);
+
+        if(validRange.includes(userGuess) == false){
+            alert('Clever, but No.\nNumber must be an integer between 1 and 10');
+        }else if(attemptNumber.includes(userGuess)){
+            alert(`You already guessed ${userGuess}.\n(I'll let it slide.)`)
+        } else if(userGuess == chosenNumber){
+            attemptNumber.push(userGuess);
+            newLog.innerText = `${(attemptNumber.length)}) Thats the number! You win!`;
+            guessLog.appendChild(newLog);
+            gameWon = true;
+            playAgain.style = 'display: block';
+            submitButton.style = 'display: none';
+        }else if(userGuess < chosenNumber){
+            attemptNumber.push(userGuess);
+            newLog.innerText = `${(attemptNumber.length)}) ${userGuess} is too low. Guess higher`;
+            guessLog.appendChild(newLog);
+        }else if(userGuess > chosenNumber){
+            attemptNumber.push(userGuess);
+            newLog.innerText = `${(attemptNumber.length)}) ${userGuess} is too high. Guess lower`;
+            guessLog.appendChild(newLog);
+        }
+        
+        console.log(attemptNumber);
+        
+        lossCheck();
+        
+    }
+   
